14f05f0e94149534fec68ce77a076cc1635ba9b3
jessada <jessada@kth>
2013-11-21 11:58:02 +0100

    done build xls for fam242 exome

---
+++ bash/bin/build_xls_for_fam242_exome.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+csvs2xls=$script_dir/csvs2xls.py
+sort_n_awk_csv=$script_dir/sort_n_awk_csv.sh
+
+project_name="fam242"
+patient_code1=Co441
+patient_code2=Co771
+patient_code3=Co666
+
+working_dir=$script_dir/tmp
+sa_db_file=$CMM_SCILIFE_ILLUMINA_SA_DB
+vcf_gz_file=$CMM_SCILIFE_ILLUMINA_ALL_PATIENTS_MANS_GZ
+tmp_sa_filtered=$working_dir/tmp_sa_filtered_for_"$project_name"
+tmp_common_mutations_csv=$working_dir/"$project_name"_common_mutations_tmp.tab.csv
+tmp_individual_mutations_csv1=$working_dir/"$project_name"_"$patient_code1"_tmp.tab.csv
+tmp_individual_mutations_csv2=$working_dir/"$project_name"_"$patient_code2"_tmp.tab.csv
+tmp_individual_mutations_csv3=$working_dir/"$project_name"_"$patient_code3"_tmp.tab.csv
+
+out_dir=$script_dir/../out/"$project_name"_exome
+out_common_mutations_csv=$out_dir/"$project_name"_common_mutations.tab.csv
+out_individual_mutations_csv1=$out_dir/"$project_name"_"$patient_code1".tab.csv
+out_individual_mutations_csv2=$out_dir/"$project_name"_"$patient_code2".tab.csv
+out_individual_mutations_csv3=$out_dir/"$project_name"_"$patient_code3".tab.csv
+out_file=$out_dir/"$project_name"_exome.xls
+
+echo "## building the xls file family 242 exome" 1>&2
+echo "## parameters" 1>&2
+echo "## summarize_annovar_file: $sa_db_file" 1>&2
+echo "## vcf gz file:            $vcf_gz_file" 1>&2
+echo "## working dir:            $working_dir" 1>&2
+echo "## out file:               $out_file" 1>&2
+echo "## maf ratio:              $maf_filter" 1>&2
+echo "## oaf ratio:              $oaf_filter" 1>&2
+
+#---------- get vcf columns from patient codes --------------
+function get_vcf_col {
+    zcat $1 | grep "^#C" | grep -i $2 | awk -va="$2" 'BEGIN{}
+    END{}
+    {
+        for(i=1;i<=NF;i++){
+            IGNORECASE = 1
+            if ( tolower($i) == tolower(a))
+                {print i }
+        }
+    }'
+}
+
+col1=`get_vcf_col $vcf_gz_file $patient_code1`
+col2=`get_vcf_col $vcf_gz_file $patient_code2`
+col3=`get_vcf_col $vcf_gz_file $patient_code3`
+#col1=14
+#col2=21
+#col3=42
+#---------- get vcf columns from patient codes --------------
+
+
+#---------- filter annotation from summarize annovar --------------
+sed -n 1p $sa_db_file > $tmp_sa_filtered
+filter_sa="grep \"^exon\" $sa_db_file | grep -vP \"\tsyn\" | awk -F'\t' '{ if (\$3 != \"unknown\" && \$3 != \"\") print \$0}'  >> $tmp_sa_filtered"
+echo "" 1>&2
+echo "## ************************** filter annotation from summarize annovar *******************************" 1>&2
+echo "## filter only exon -> remove synonymous SNV -> remove all \"unknown\" and \"\" in ExonicFunc" 1>&2
+echo "## executing $filter_sa" 1>&2
+eval $filter_sa
+#---------- filter annotation from summarize annovar --------------
+
+function join_sa_vcf_n_filter {
+    vcf_keys_file=$1
+    filtered_sa_file=$2
+    tmp_dir=$3
+
+    tmp_join_sa_vcf=$tmp_dir/tmp_join_sa_vcf
+
+    join_sa_vcf_cmd="join -t $'\t' -1 1 -2 1 -o 2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,2.10,2.11,2.12,2.13,2.14,2.15,2.16,2.17,2.18,2.19,2.20,2.21,2.22,2.23,2.24,2.25,2.26,2.27,2.28,2.29,2.30,2.31,2.32,2.33,2.34 <( awk -F'\t' '{ if (\$1 !~ /X/ && \$1 !~ /Y/ && \$1 !~ /MT/) print \$0 }' $vcf_keys_file | sort -k1) <( awk -F '\t' '{ printf \"%s\t%s\n\", \$32, \$0 }' $filtered_sa_file | awk -F'\t' '{ if (\$1 !~ /X/ && \$1 !~ /Y/ && \$1 !~ /MT/) print \$0 }' | sort -k1) | sort -n -k32 -t\$'\t' > $tmp_join_sa_vcf"
+    echo "" 1>&2
+    echo "## executing $join_sa_vcf_cmd" 1>&2
+    eval $join_sa_vcf_cmd
+    join_sa_vcf_cmd="join -t $'\t' -1 1 -2 1 -o 2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,2.10,2.11,2.12,2.13,2.14,2.15,2.16,2.17,2.18,2.19,2.20,2.21,2.22,2.23,2.24,2.25,2.26,2.27,2.28,2.29,2.30,2.31,2.32,2.33,2.34 <( awk -F'\t' '{ if (\$1 ~ /X/ || \$1 ~ /Y/ || \$1 ~ /MT/) print \$0 }' $vcf_keys_file | sort -k1) <( awk -F '\t' '{ printf \"%s\t%s\n\", \$32, \$0 }' $filtered_sa_file | awk -F'\t' '{ if (\$1 ~ /X/ || \$1 ~ /Y/ || \$1 ~ /MT/) print \$0 }' | sort -k1) | sort -n -k32 -t\$'\t' >> $tmp_join_sa_vcf"
+    echo "## executing $join_sa_vcf_cmd" 1>&2
+    eval $join_sa_vcf_cmd
+
+    cat $tmp_join_sa_vcf
+}
+
+function build_common_mutations_csv {
+    gz_file=$1
+    sa_file=$2
+    tmp_dir=$3
+    col1=$4
+    col2=$5
+    col3=$6
+
+    tmp_vcf_keys=$tmp_dir/tmp_vcf_keys_for_fam24_exome
+
+    echo "## ************************** Build commmon mutations *******************************" 1>&2
+    generate_vcf_keys_cmd="zcat $gz_file | grep -v \"^#\" | awk -F'\t' '{ if ((\$$col1 != \"./.\" && \$$col1 !~ \"0/0\") && (\$$col2 != \"./.\" && \$$col2 !~ \"0/0\") && (\$$col3 != \"./.\" && \$$col3 !~ \"0/0\")) printf \"%s|%012d\t%s\t%s\t%s\n\", \$1, \$2, \$$col1, \$$col2, \$$col3 }' > $tmp_vcf_keys"
+    echo "## generate vcf keys for all mutations that there are mutations in any members of family 242" 1>&2
+    echo "## executing $generate_vcf_keys_cmd" 1>&2
+    eval $generate_vcf_keys_cmd
+
+    join_sa_vcf_n_filter $tmp_vcf_keys $sa_file $tmp_dir
+}
+
+function build_individual_mutations_csv {
+    gz_file=$1
+    sa_file=$2
+    tmp_dir=$3
+    col=$4
+
+    tmp_vcf_keys=$tmp_dir/tmp_vcf_keys_for_fam24_exome_"$col"
+
+    generate_vcf_keys_cmd="zcat $gz_file | grep -v \"^#\" | awk -F'\t' '{ if (\$$col != \"./.\" && \$$col !~ \"0/0\") printf \"%s|%012d\t%s\n\", \$1, \$2, \$$col }' > $tmp_vcf_keys"
+    echo "## ************************** Build individual mutations (col $col)*******************************" 1>&2
+    echo "## generate vcf keys for individual mutations" 1>&2
+    echo "## executing $generate_vcf_keys_cmd" 1>&2
+    eval $generate_vcf_keys_cmd
+
+    join_sa_vcf_n_filter $tmp_vcf_keys $sa_file $tmp_dir
+}
+
+echo "" 1>&2
+echo "## gerating csv for common mutation of family 242" 1>&2
+sed -n 1p $sa_db_file > $tmp_common_mutations_csv
+build_common_mutations_csv $vcf_gz_file $tmp_sa_filtered $working_dir $col1 $col2 $col3 >> $tmp_common_mutations_csv
+echo "" 1>&2
+echo "## gerating csv for individual mutation of patient $patient_code1" 1>&2
+sed -n 1p $sa_db_file > $tmp_individual_mutations_csv1
+build_individual_mutations_csv $vcf_gz_file $tmp_sa_filtered $working_dir $col1 >> $tmp_individual_mutations_csv1
+echo "" 1>&2
+echo "## gerating csv for individual mutation of patient $patient_code2" 1>&2
+sed -n 1p $sa_db_file > $tmp_individual_mutations_csv2
+build_individual_mutations_csv $vcf_gz_file $tmp_sa_filtered $working_dir $col2 >> $tmp_individual_mutations_csv2
+echo "" 1>&2
+echo "## gerating csv for individual mutation of patient $patient_code3" 1>&2
+sed -n 1p $sa_db_file > $tmp_individual_mutations_csv3
+build_individual_mutations_csv $vcf_gz_file $tmp_sa_filtered $working_dir $col3 >> $tmp_individual_mutations_csv3
+echo "" 1>&2
+
+$sort_n_awk_csv $tmp_common_mutations_csv > $out_common_mutations_csv
+$sort_n_awk_csv $tmp_individual_mutations_csv1 > $out_individual_mutations_csv1
+$sort_n_awk_csv $tmp_individual_mutations_csv2 > $out_individual_mutations_csv2
+$sort_n_awk_csv $tmp_individual_mutations_csv3 > $out_individual_mutations_csv3
+
+python $csvs2xls $out_individual_mutations_csv1 $patient_code1 $out_individual_mutations_csv2 $patient_code2 $out_individual_mutations_csv3 $patient_code3 $out_common_mutations_csv $out_file
---
+++ bash/bin/csvs2xls.py
@@ -0,0 +1,124 @@
+import sys
+import csv
+import xlwt
+
+member1_csv  = sys.argv[1]
+member1_code = sys.argv[2]
+
+if len(sys.argv) == 4:
+    output_file  = sys.argv[3]
+else:
+    member2_csv  = sys.argv[3]
+    member2_code = sys.argv[4]
+    if len(sys.argv) == 7:
+        incommon_csv = sys.argv[5]
+        output_file  = sys.argv[6]
+    else:
+        member3_csv  = sys.argv[5]
+        member3_code = sys.argv[6]
+        incommon_csv = sys.argv[7]
+        output_file  = sys.argv[8]
+
+def isFloat(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+def explain_annotation(csv_record):
+    col_ljb_phylop_pred         = 13
+    col_ljb_sift_pred           = 15
+    col_ljb_polyphen2_pred      = 17
+    col_ljb_lrt_pred            = 19
+    col_ljb_mutationtaster_pred = 21
+
+    phylop_explanation         = {'C': 'conserved', 'N': 'not conserved'}
+    sift_explanation           = {'T': 'tolerated', 'D': 'deleterious'}
+    polyphen2_explanation      = {'D': 'probably damaging', 'P': 'possibly damaging', 'B': 'benign'}
+    lrt_explanation            = {'D': 'tolerated', 'N': 'neutral'}
+    mutationtaster_explanation = {'A': 'disease causing automatic', 'D': 'disease causing', 'N': 'polymorphism', 'P': 'polymorphism automatic'}
+
+#    print csv_record[col_ljb_phylop_pred], csv_record[col_ljb_sift_pred], csv_record[col_ljb_lrt_pred]
+    if csv_record[col_ljb_phylop_pred] in phylop_explanation:
+        csv_record[col_ljb_phylop_pred]         = phylop_explanation[csv_record[col_ljb_phylop_pred]]
+    if csv_record[col_ljb_sift_pred] in sift_explanation:
+        csv_record[col_ljb_sift_pred]           = sift_explanation[csv_record[col_ljb_sift_pred]]
+    if csv_record[col_ljb_polyphen2_pred] in polyphen2_explanation:
+        csv_record[col_ljb_polyphen2_pred]      = polyphen2_explanation[csv_record[col_ljb_polyphen2_pred]]
+    if csv_record[col_ljb_lrt_pred] in lrt_explanation:
+        csv_record[col_ljb_lrt_pred]            = lrt_explanation[csv_record[col_ljb_lrt_pred]]
+    if csv_record[col_ljb_mutationtaster_pred] in mutationtaster_explanation:
+        csv_record[col_ljb_mutationtaster_pred] = mutationtaster_explanation[csv_record[col_ljb_mutationtaster_pred]]
+    return csv_record
+
+def split_last_extra_info(csv_record, sheet_name):
+#    tmp_len   = len(csv_record)
+#    if sheet_name != "In common":
+#        if csv_record[tmp_len-1] == 'Otherinfo':
+#            csv_record.append('QUAL')
+#            csv_record.append('FILTER')
+#        else:
+#            tmp_split = csv_record[tmp_len-1].split('|')
+#            csv_record[tmp_len-1] = tmp_split[0]
+#            csv_record.append(tmp_split[1])
+#            csv_record.append(tmp_split[2])
+#    else:
+#        csv_record[tmp_len-3] = csv_record[tmp_len-3].split('|')[0]
+#        csv_record[tmp_len-2] = csv_record[tmp_len-2].split('|')[0]
+#        csv_record[tmp_len-1] = csv_record[tmp_len-1].split('|')[0]
+    return csv_record
+
+
+#def add_csv_sheet(wb, sheet_name, csv_file, st):
+#    ws = wb.add_sheet(sheet_name)
+#    with open(csv_file, 'rb') as csvfile:
+#        csv_records = list(csv.reader(csvfile, delimiter='\t'))
+#        for row in xrange(len(csv_records)):
+#            csv_record = csv_records[row]
+#            csv_record = split_last_extra_info(explain_annotation(csv_record), sheet_name)
+#            for col in xrange(len(csv_record)):
+#                if (isFloat(csv_record[7]) and (float(csv_record[7])<=0.1)) or (csv_record[7]=='') :
+#                    if (csv_record[2] != 'synonymous SNV'):
+#                        ws.write(row, col, csv_record[col], st)
+#                    else:
+#                        ws.write(row, col, csv_record[col])
+#                else:
+#                    ws.write(row, col, csv_record[col])
+#    ws.set_panes_frozen(True)
+#    ws.set_horz_split_pos(1)
+#    ws.set_remove_splits(True)
+#
+def add_csv_sheet(wb, sheet_name, csv_file, st):
+    ws = wb.add_sheet(sheet_name)
+    with open(csv_file, 'rb') as csvfile:
+        csv_records = list(csv.reader(csvfile, delimiter='\t'))
+        csv_row = 0
+        for xls_row in xrange(len(csv_records)):
+            csv_record = csv_records[xls_row]
+#            if (isFloat(csv_record[7]) and (float(csv_record[7]) > 0.49)):
+#                continue
+            csv_record = split_last_extra_info(explain_annotation(csv_record), sheet_name)
+            for col in xrange(len(csv_record)):
+                if (isFloat(csv_record[7]) and (float(csv_record[7])<=0.1)) or (csv_record[7]=='') :
+                    ws.write(csv_row, col, csv_record[col])
+#                    ws.write(csv_row, col, csv_record[col], st)
+                else:
+                    ws.write(csv_row, col, csv_record[col])
+            csv_row += 1
+    ws.set_panes_frozen(True)
+    ws.set_horz_split_pos(1)
+    ws.set_remove_splits(True)
+
+wb = xlwt.Workbook()
+yellow_st = xlwt.easyxf('pattern: pattern solid, fore_colour yellow;')
+
+add_csv_sheet(wb, member1_code, member1_csv, yellow_st)
+if len(sys.argv) > 4:
+    add_csv_sheet(wb, member2_code, member2_csv, yellow_st)
+    if len(sys.argv) == 9:
+        add_csv_sheet(wb, member3_code, member3_csv, yellow_st)
+    add_csv_sheet(wb, "In common", incommon_csv, yellow_st)
+
+wb.save(output_file)
+
