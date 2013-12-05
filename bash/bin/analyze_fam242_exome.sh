e54300d4042e081d32bb127a7fe6f56c30e7bb9f
jessada <jessada@kth>
2013-12-05 10:04:48 +0100

    add analyze_other_uppsala_50fam to fam242_exome

--- bash/bin/analyze_fam242_exome.sh
+++ bash/bin/analyze_fam242_exome.sh
@@ -11,20 +11,23 @@ patient_code[3]=Co666
 
 working_dir=$script_dir/tmp
 sa_db_file=$CMM_SCILIFE_ILLUMINA_MANS_SA_DB
-vcf_gz_file=$CMM_SCILIFE_ILLUMINA_ALL_PATIENTS_MANS_GZ
-vcf_header_file=$CMM_SCILIFE_ILLUMINA_ALL_PATIENTS_MANS_HEADER
+
 oaf_file=$CMM_OAF_SCILIFE_ILLUMINA_ALL_FAM
 
+other_families_sub_project_name[1]="scilife_illumina_families"
+vcf_gz_file[1]=$CMM_SCILIFE_ILLUMINA_ALL_PATIENTS_MANS_GZ
+vcf_header_file[1]=$CMM_SCILIFE_ILLUMINA_ALL_PATIENTS_MANS_HEADER
+other_families_sub_project_name[2]="uppsala_50fam"
+vcf_gz_file[2]=$CMM_UPPSALA_50FAM_ALL_PATIENTS_GZ
+vcf_header_file[2]=$CMM_UPPSALA_50FAM_ALL_PATIENTS_HEADER
+
 out_dir=$script_dir/../out/"$project_name"
-out_other_families_csv=$out_dir/"$project_name"_analyze_other_families.csv
 out_oaf_csv=$out_dir/"$project_name"_analyze_oaf.csv
 out_file=$out_dir/"$project_name"_analyze.xls
 
-echo "## analyze result from family 242 exome" 1>&2
+echo "## analyze result from $project_name" 1>&2
 echo "## parameters" 1>&2
 echo "## summarize_annovar_file: $sa_db_file" 1>&2
-echo "## vcf gz file:            $vcf_gz_file" 1>&2
-echo "## vcf header file:        $vcf_header_file" 1>&2
 echo "## working dir:            $working_dir" 1>&2
 echo "## out file:               $out_file" 1>&2
 
@@ -45,13 +48,6 @@ function get_vcf_col {
 
 echo "## family & members" 1>&2
 echo "## family code:            $project_name" 1>&2
-
-for (( i=1; i<=$((${#patient_code[@]})); i++ ))
-do
-    patient_col[$i]=`get_vcf_col $vcf_header_file ${patient_code[$i]}`
-    echo "## patient code$i:          ${patient_code[$i]}" 1>&2
-    echo "## column:                 ${patient_col[$i]}" 1>&2
-done
 
 gene_name[1]="WDR63"
 gene_name[2]="EPB41L4B"
@@ -319,101 +315,111 @@ function cut_gt {
     eval $cut_cmd
 }
 
-printf_str=""
-patient_param=""
-for (( i=1; i<=$((${#patient_code[@]})); i++ ))
-do
-    printf_str+="\t%s"
-    patient_param+=", \$${patient_col[$i]}"
-done
+function lookup_mt_in_other_families {
+    vcf_gz_file=$1
+    vcf_header_file=$2
 
-n_col=`awk -F '\t' '{print NF}' $vcf_header_file`
+    for (( i=1; i<=$((${#patient_code[@]})); i++ ))
+    do
+        patient_col[$i]=`get_vcf_col $vcf_header_file ${patient_code[$i]}`
+        echo "## patient code$i:          ${patient_code[$i]}" 1>&2
+        echo "## column:                 ${patient_col[$i]}" 1>&2
+    done
+    tmp_csv="$working_dir/tmp.csv"
 
-parsed_header="gene\tchrom\tposition"
-case_header=""
-non_case_header=""
-for (( j=1; j<=$n_col; j++ ))
-do
-    if [ $j -ne 1 ] && [ $j -ne 2 ] && [ $j -ne 3 ] && [ $j -ne 6 ] && [ $j -ne 7 ] && [ $j -ne 8 ] && [ $j -ne 9 ]; then
-        case=0
-        for (( k=1; k<=$((${#patient_code[@]})); k++ ))
-        do
-            if [ $j -eq ${patient_col[$k]} ]; then
-                case=$j
-            fi
-        done
-        if [ $case -ne 0 ]; then
-            cut_cmd="cut -f$case $vcf_header_file"
-            case_header+="\t"`eval $cut_cmd`
-            #case_header+="\t"`cut_gt "$rec" "$case"`
-        elif [ $j -gt 9 ]; then
-            cut_cmd="cut -f$j $vcf_header_file"
-            non_case_header+="\t"`eval $cut_cmd`
-#            non_case_header+="\t"`cut_gt "$rec" "$j"`
-        else
-            cut_cmd="cut -f$j $vcf_header_file"
-            parsed_header+="\t"`eval $cut_cmd`
-#            parsed_header+="\t"`cut_gt "$rec" "$j"`
-        fi
-#        echo_cmd="cut -f$j $vcf_header_file"
-#        gt=`eval $echo_cmd`
-#        parsed_header+="\t$gt"
-    fi
-done
-parsed_header+="$case_header"
-parsed_header+="$non_case_header"
-echo -e "$parsed_header" > $out_other_families_csv
+    printf_str=""
+    patient_param=""
+    for (( i=1; i<=$((${#patient_code[@]})); i++ ))
+    do
+        printf_str+="\t%s"
+        patient_param+=", \$${patient_col[$i]}"
+    done
 
-for (( i=1; i<=$((${#gene_pos[@]})); i++ ))
-do
-    dbg_cmd="tabix $vcf_gz_file ${chrom[$i]}":"${gene_pos[$i]}"-"${gene_pos[$i]}"
-#    dbg_cmd="tabix $vcf_gz_file $chrom":"${gene_pos[$i]}"-"${gene_pos[$i]} | awk -F'\t' '{ printf \"${gene_name[$i]}\t%s\t%s$printf_str\n\", \$1, \$2$patient_param }'"
-#    echo "executing $dbg_cmd" 1>&2
-#    eval $dbg_cmd
-    rec=`eval $dbg_cmd`
-#    echo "$rec" 1>&2
-    parsed_rec="${gene_name[$i]}"
-#    echo_cmd="echo \"$rec\" | cut -f1,2,4,5 -d\" \""
-#    parsed_rec+=`eval $echo_cmd`
-    case_gts=""
-    non_case_gts=""
+    n_col=`awk -F '\t' '{print NF}' $vcf_header_file`
+
+    parsed_header="gene\tchrom\tposition"
+    case_header=""
+    non_case_header=""
     for (( j=1; j<=$n_col; j++ ))
     do
-        if [ $j -ne 3 ] && [ $j -ne 6 ] && [ $j -ne 7 ] && [ $j -ne 8 ] && [ $j -ne 9 ]; then
+        if [ $j -ne 1 ] && [ $j -ne 2 ] && [ $j -ne 3 ] && [ $j -ne 6 ] && [ $j -ne 7 ] && [ $j -ne 8 ] && [ $j -ne 9 ]; then
             case=0
             for (( k=1; k<=$((${#patient_code[@]})); k++ ))
             do
-                if [ $j -eq ${patient_col[$k]} ]; then
-                    case=$j
+                if [ -n "${patient_col[$k]}" ]; then
+                    if [ $j -eq ${patient_col[$k]} ]; then
+                        case=$j
+                    fi
                 fi
             done
             if [ $case -ne 0 ]; then
-                case_gts+="\t"`cut_gt "$rec" "$case"`
+                cut_cmd="cut -f$case $vcf_header_file"
+                case_header+="\t"`eval $cut_cmd`
             elif [ $j -gt 9 ]; then
-                non_case_gts+="\t"`cut_gt "$rec" "$j"`
+                cut_cmd="cut -f$j $vcf_header_file"
+                non_case_header+="\t"`eval $cut_cmd`
             else
-                parsed_rec+="\t"`cut_gt "$rec" "$j"`
+                cut_cmd="cut -f$j $vcf_header_file"
+                parsed_header+="\t"`eval $cut_cmd`
             fi
-#            parsed_rec+="\t"`cut_gt "$rec" "$j"`
         fi
     done
-    parsed_rec+="$case_gts"
-    parsed_rec+="$non_case_gts"
-    echo -e "$parsed_rec" >> $out_other_families_csv
-#    for (( j=1; j<=$n_col; j++ ))
-#    do
-#        if [ $j -ne 3 ] && [ $j -ne 6 ] && [ $j -ne 7 ] && [ $j -ne 8 ] && [ $j -ne 9 ]; then
-#            echo_cmd="echo \"$rec\" | cut -f$j -d\" \" | cut -f1 -d\":\""
-#    #        echo_cmd="$dbg_cmd | cut -f$j"
-#    #        echo "executing $echo_cmd" 1>&2
-#    #        gt[$j]=`eval $echo_cmd`
-#            gt=`eval $echo_cmd`
-#            parsed_rec+="\t$gt"
-#        fi
-#    done
-#    echo -e "$parsed_rec" >> $out_other_families_csv
+    parsed_header+="$case_header"
+    parsed_header+="$non_case_header"
+    echo -e "$parsed_header" > $tmp_csv
+
+    for (( i=1; i<=$((${#gene_pos[@]})); i++ ))
+    do
+        dbg_cmd="tabix $vcf_gz_file ${chrom[$i]}":"${gene_pos[$i]}"-"${gene_pos[$i]}"
+        rec=`eval $dbg_cmd`
+        parsed_rec="${gene_name[$i]}"
+        case_gts=""
+        non_case_gts=""
+        for (( j=1; j<=$n_col; j++ ))
+        do
+            if [ $j -ne 3 ] && [ $j -ne 6 ] && [ $j -ne 7 ] && [ $j -ne 8 ] && [ $j -ne 9 ]; then
+                case=0
+                for (( k=1; k<=$((${#patient_code[@]})); k++ ))
+                do
+                    if [ -n "${patient_col[$k]}" ]; then
+                        if [ $j -eq ${patient_col[$k]} ]; then
+                            case=$j
+                        fi
+                    fi
+                done
+                if [ $case -ne 0 ]; then
+                    case_gts+="\t"`cut_gt "$rec" "$case"`
+                elif [ $j -gt 9 ]; then
+                    non_case_gts+="\t"`cut_gt "$rec" "$j"`
+                else
+                    parsed_rec+="\t"`cut_gt "$rec" "$j"`
+                fi
+            fi
+        done
+        parsed_rec+="$case_gts"
+        parsed_rec+="$non_case_gts"
+        echo -e "$parsed_rec" >> $tmp_csv
+    done
+
+    cat $tmp_csv
+}
+
+csv2xls_cmd="python $csvs2xls_highlight_MT $out_file"
+
+for (( n=1; n<=$((${#other_families_sub_project_name[@]})); n++ ))
+do
+    csv_file=$out_dir/"$project_name"_analyze_other_${other_families_sub_project_name[$n]}.csv
+    echo "" 1>&2
+    echo "## sub project name:       ${other_families_sub_project_name[$n]}" 1>&2
+    echo "## vcf file:               ${vcf_gz_file[$n]}" 1>&2
+    echo "## header file:            ${vcf_header_file[$n]}" 1>&2
+    lookup_mt_in_other_families ${vcf_gz_file[$n]} ${vcf_header_file[$n]} $scilife_illumina_mans_vcf_header_file > $csv_file
+
+    csv2xls_cmd+=" $csv_file ${other_families_sub_project_name[$n]}"
 done
+
 #---------- debug other families --------------
+
 #---------- debug oaf --------------
 echo -e "chrom\tstart pos\tend pos\tref\tobs\toaf" > $out_oaf_csv
 for (( i=1; i<=$((${#gene_pos[@]})); i++ ))
@@ -423,6 +429,6 @@ do
 done
 #---------- debug oaf --------------
 
-csv2xls_cmd="python $csvs2xls_highlight_MT $out_file $out_other_families_csv \"other families\" $out_oaf_csv \"oaf\""
+csv2xls_cmd+=" $out_oaf_csv \"oaf\""
+echo "executing $csv2xls_cmd" 1>&2
 eval $csv2xls_cmd
-
