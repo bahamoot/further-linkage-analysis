3c92d5fef14746b464f6531b3f52f47a525d0d70
jessada <jessada@kth>
2014-01-10 12:10:26 +0100

    re-add explanation for effect prediction in csv2xls.py

--- bash/bin/gen_xls_for_sanna_n_vinay_projs.sh
+++ bash/bin/gen_xls_for_sanna_n_vinay_projs.sh
@@ -2,7 +2,7 @@
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-script_build_xls=$script_dir/script_build_xls_for_sanna_n_vinay.sh
+script_gen_xls=$script_dir/script_gen_xls_for_sanna_n_vinay.sh
 working_dir=$script_dir/tmp
 
 #---------- recessive model colon cancer --------------
@@ -11,9 +11,9 @@ min_pos="37763732" #rs1738240 = 38763732
 max_pos="45479949" #rs941983  = 44479949
 sub_project_name="colon_recessive_chr$chrom"
 
-$script_build_xls $sub_project_name $chrom $min_pos $max_pos "110" "1526-02D" "Co1301"
-$script_build_xls $sub_project_name $chrom $min_pos $max_pos "301" "Co837" "Co840" "Co1053"
-$script_build_xls $sub_project_name $chrom $min_pos $max_pos "650" "398-05o" "729-05o"
+$script_gen_xls $sub_project_name $chrom $min_pos $max_pos "110" "1526-02D" "Co1301"
+$script_gen_xls $sub_project_name $chrom $min_pos $max_pos "301" "Co837" "Co840" "Co1053"
+$script_gen_xls $sub_project_name $chrom $min_pos $max_pos "650" "398-05o" "729-05o"
 
 #---------- recessive model rectal cancer --------------
 chrom="18"
@@ -21,8 +21,8 @@ min_pos="9928706"  #rs906283  = 10928706
 max_pos="21607819" #rs1010800 = 20607819
 sub_project_name="rectal_recessive_chr$chrom"
 
-$script_build_xls $sub_project_name $chrom $min_pos $max_pos "8" "Co35" "Co37"
-#$script_build_xls $sub_project_name $chrom $min_pos $max_pos "258" "" ""
-$script_build_xls $sub_project_name $chrom $min_pos $max_pos "918" "134-06" "354-06"
-$script_build_xls $sub_project_name $chrom $min_pos $max_pos "1213" "Co1666"
+$script_gen_xls $sub_project_name $chrom $min_pos $max_pos "8" "Co35" "Co37"
+#$script_gen_xls $sub_project_name $chrom $min_pos $max_pos "258" "" ""
+$script_gen_xls $sub_project_name $chrom $min_pos $max_pos "918" "134-06" "354-06"
+$script_gen_xls $sub_project_name $chrom $min_pos $max_pos "1213" "Co1666"
 
